7d8601cdecb73e7d220a2099fc9a69c0e3be805b
asiasmol <joanna.smolinska.cool@gmail.com>
2023-05-22 11:48:40 +0200

    improvment validatePassword special characters can be used

--- src/components/registration/UserRegistration.tsx
+++ src/components/registration/UserRegistration.tsx
@@ -65,9 +65,10 @@ const UserRegistration = () => {
     };
 
     const validatePassword = (password: string) => {
-        const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+        const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}$/;
         return passwordRegex.test(password);
     }
+
 
 
     const onFirstnameChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
